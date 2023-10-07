33151b77b81340863d91fad92c4a8db343379b72
chamny20 <chamny20@naver.com>
2023-10-07 18:01:36 +0900

    chore: add file

---
+++ src/page/Home/VolunteerPage.tsx
@@ -0,0 +1,11 @@
+import Category from './category';
+
+const VolunteerPage = () => {
+  return (
+    <>
+      <Category />
+    </>
+  );
+};
+
+export default VolunteerPage;
---
+++ src/page/Home/category.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Category = () => {
+  return (
+    <CategoryContainer>
+      {/* <div>{title}</div> */}
+      <div>hi</div>
+      <div>ㅇㅇㅇddd</div>
+    </CategoryContainer>
+  );
+};
+
+const CategoryContainer = styled.div`
+  display: flex;
+  border: 1px solid red;
+`;
+
+export default Category;

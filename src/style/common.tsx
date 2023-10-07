bf59c249a867c69aabab86c342d210069c34a4c0
aaminha <dks4857@gmail.com>
2023-10-07 14:45:56 +0900

    design: 폰트 세부 설정

---
+++ src/style/common.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Hl1 = styled.div`
+  font-family: Pretendard;
+  font-size: 64px;
+  font-weight: 700;
+  line-height: 76px;
+`;
+
+export const Hl2 = styled.div`
+  font-family: Pretendard;
+  font-size: 36px;
+  font-weight: 700;
+  line-height: 44px;
+`;
+
+export const Hl3 = styled.div`
+  font-family: Pretendard;
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 36px;
+`;
+
+export const Hl4 = styled.div`
+  font-family: Pretendard;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 28px;
+`;
+
+export const B1 = styled.div`
+  font-family: Pretendard;
+  font-size: 18px;
+  line-height: 28px;
+`;
+
+export const B2 = styled.div`
+  font-family: Pretendard;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const B3 = styled.div`
+  font-family: Pretendard;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const B4 = styled.div`
+  font-family: Pretendard;
+  font-size: 12px;
+  line-height: 16px;
+`;

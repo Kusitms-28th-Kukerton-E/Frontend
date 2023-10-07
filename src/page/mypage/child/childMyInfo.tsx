b92fb05f61f08e760bd49495506e648b77a41adb
chamny20 <chamny20@naver.com>
2023-10-08 03:40:36 +0900

    fix: unused code 제거

--- src/page/mypage/child/childMyInfo.tsx
+++ src/page/mypage/child/childMyInfo.tsx
@@ -56,18 +56,6 @@ const InfoTop = styled.div`
   background: var(--Main-color2, #498dfd);
 `;
 
-const InfoBottom = styled.div`
-  /* border: 1px solid red; */
-  display: flex;
-
-  border-bottom-left-radius: 40px;
-  border-bottom-right-radius: 40px;
-  padding: 80px 80px;
-
-  border: 5px solid var(--Main-color2, #498dfd);
-  background: var(--White, #fff);
-`;
-
 const InfoSmall = styled.div`
   /* border: 1px solid red; */
   display: grid;
@@ -100,28 +88,6 @@ const InfoButton = styled.div`
   line-height: 150%; /* 39px */
 `;
 
-const InfoButtonBottom = styled.div`
-  width: 160px;
-  height: 50px;
-  flex-shrink: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: black;
-  border-radius: 30px;
-  background: var(--Grey-blue, #abbed1);
-
-  color: var(--Black, #263238);
-  font-feature-settings:
-    'clig' off,
-    'liga' off;
-  font-family: Pretendard;
-  font-size: 26px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 150%; /* 39px */
-`;
-
 const NameBox = styled.div`
   display: flex;
   gap: 23px;
@@ -138,26 +104,6 @@ const InfoText = styled.div`
   font-style: normal;
   font-weight: 600;
   line-height: 120%; /* 38.4px */
-`;
-
-const InfoTextBottom = styled.div`
-  width: 100px;
-  height: 50px;
-  flex-shrink: 0;
-  border-radius: 30px;
-  border: 3px solid var(--Main-color2, #498dfd);
-  background: var(--White, #fff);
-  color: var(--Black, #263238);
-  text-align: center;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  /* 본문1 */
-  font-family: Pretendard;
-  font-size: 26px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 150%; /* 39px */
 `;
 
 export default ChildMyInfo;
